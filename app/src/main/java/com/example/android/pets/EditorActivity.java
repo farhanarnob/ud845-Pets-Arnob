294e78e3a61e6515f26a1e4c1db5cd12776f5d26
Farhan Rahman Arnob <farhanarnob@live.com>
2017-06-21 00:47:18 +0600

    Content Provider created.

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -126,7 +126,7 @@ public class EditorActivity extends AppCompatActivity {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
                 if (insertPet() != -1) {
-
+                    finish();
                 }
                 return true;
             // Respond to a click on the "Delete" menu option
