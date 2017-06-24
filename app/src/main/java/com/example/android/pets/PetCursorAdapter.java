6540fc09363e9da0c85c70af68ca98416c48b50a
Farhan Rahman Arnob <farhanarnob@live.com>
2017-06-25 02:15:57 +0600

    Cursor Loader has been implemented.

---
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.pets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract;
+
+/**
+ * Created by ${farhanarnob} on ${06-Oct-16}.
+ */
+
+public class PetCursorAdapter extends CursorAdapter {
+
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.item_list, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView tvName = (TextView) view.findViewById(R.id.name);
+        TextView tvSummary = (TextView) view.findViewById(R.id.summary);
+
+        String cName = cursor.getString(cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME));
+        String cSummary = cursor.getString(cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED));
+
+        tvName.setText(cName);
+        tvSummary.setText(cSummary);
+    }
+}
